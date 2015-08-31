816e86cb2c9ea02ed5ce2154c60a08e0994631d2
dannyshafer <github@danielshafer.com>
2015-08-31 12:06:08 -0700

    skeleton shop

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,42 @@
+class OrdersController < ApplicationController
+
+  before_filter(only: [:paypal, :payment]) { Shoppe::Paypal.setup_paypal }
+
+  def paypal
+    @order = Shoppe::Order.find(session[:current_order_id])
+    url = @order.redirect_to_paypal(checkout_payment_url(success: true), checkout_payment_url(success: false))
+    redirect_to url
+  end
+
+  def destroy
+    current_order.destroy
+    session[:order_id] = nil
+    redirect_to root_path, :notice => "Basket emptied successfully."
+  end
+
+  def checkout
+    @order = Shoppe::Order.find(current_order.id)
+    if request.patch?
+      if @order.proceed_to_confirm(params[:order].permit(:first_name, :last_name, :billing_address1, :billing_address2, :billing_address3, :billing_address4, :billing_country_id, :billing_postcode, :email_address, :phone_number))
+        redirect_to checkout_payment_path
+      end
+    end
+  end
+
+  def payment
+    if request.post?
+      redirect_to checkout_confirmation_path
+    end
+    if params[:success] == "true" && params[:PayerID].present?
+      @order.accept_paypal_payment(params[:paymentId], params[:token], params[:PayerID])
+    end
+  end
+
+  def confirmation
+    if request.post?
+      current_order.confirm!
+      session[:order_id] = nil
+      redirect_to root_path, :notice => "Order has been placed successfully!"
+    end
+  end
+end
